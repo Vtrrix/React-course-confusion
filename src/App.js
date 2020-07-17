5cd6d8f9d3712e70a2a6b49136c07f05576d0035
VIVEK SONY <45513498+Vtrrix@users.noreply.github.com>
2020-07-17 21:29:20 +0100

    reaact redux form added next topic form validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
 import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
 
 import "./App.css";
+const store = ConfigureStore();
 
 class app extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<BrowserRouter>
-				<div className="App">
-					<Main />
-				</div>
-			</BrowserRouter>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default app;
